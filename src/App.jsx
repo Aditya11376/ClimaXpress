d8d79ee1e51a0ff8755256248f9159c23b261f5c
Aditya gupta <iamaditya1.gupta@gmail.com>
2025-10-18 12:05:29 +0530

    Update App.jsx and vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { AppLayout } from "./components/Layout/AppLayout.jsx";
 import { Home } from "./pages/Home.jsx";
 import { About } from "./pages/About.jsx";
@@ -6,20 +6,34 @@ import { Search } from "./pages/Search.jsx";
 import { Contact } from "./pages/Contact.jsx";
 import { ErrorPage } from "./pages/ErrorPage.jsx";
 
-const App = () => {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="search" element={<Search />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
-    </HashRouter>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+      {
+        path: "search", 
+        element: <Search />
+      },
+      {
+        path: "contact",
+        element: <Contact />
+      },
+    ]
+  }
+]);
+
+const App = () =>{
+  return <RouterProvider router={router} />;
+}
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ClimaXpress/', //repo name
+  base: '/ClimaXpress/',  
 })
